0ae57650a5cb13ca6c88469ef2603537aab55cbf
Moritz Meier <moritzmeier@aol.com>
2017-08-10 16:09:35 +0200

    set hy alias for line and cell magic

--- hymagic/__init__.py
+++ hymagic/__init__.py
@@ -9,20 +9,19 @@ from hy.lex import LexException, PrematureEndOfInput, tokenize
 from hy.compiler import hy_compile, HyTypeError
 from hy.importer import ast_compile, import_buffer_to_module
 from hy.completer import completion
-
 from hy.macros import macro, require
 from hy import HyExpression, HyString, HySymbol
-
 from hy._compat import builtins
+
+from IPython.core import magic
+
 
 SIMPLE_TRACEBACKS = True
 
 
-from IPython.core.magic import Magics, magics_class, line_cell_magic
 
-
-@magics_class
-class HylangMagics(Magics):
+@magic.magics_class
+class HylangMagics(magic.Magics):
     """Magic for the hylang lisp language
     """
     def __init__(self, shell):
@@ -33,17 +32,17 @@ class HylangMagics(Magics):
 
         """
         super(HylangMagics, self).__init__(shell)
-    
-    @line_cell_magic
+
+    @magic.line_cell_magic
     def hylang(self, line, cell=None, filename='<input>', symbol='single'):
         """ Ipython magic function for running hylang code in ipython
         Use %hylang one line of code or
             %%hylang for a block or cell
             Note that we pass the AST directly to IPython."""
-        
+
         global SIMPLE_TRACEBACKS
         source = cell if cell else line
-        
+
         try:
             tokens = tokenize(source)
         except PrematureEndOfInput:
@@ -53,7 +52,7 @@ class HylangMagics(Magics):
                 e.source = source
                 e.filename = filename
             print(str(e))
-        
+
         try:
             _ast = hy_compile(tokens, "__console__", root=ast.Interactive)
             self.shell.run_ast_nodes(_ast.body,'<input>',compiler=ast_compile)
@@ -71,3 +70,6 @@ class HylangMagics(Magics):
 def load_ipython_extension(ip):
     """Load the extension in IPython."""
     ip.register_magics(HylangMagics)
+    # magic_kind='line_cell' is not supported any more for some reason
+    ip.magics_manager.register_alias('hy', 'hylang', magic_kind='line')
+    ip.magics_manager.register_alias('hy', 'hylang', magic_kind='cell')
